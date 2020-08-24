a8e22ed16544216fbc22401f7efb19284d14e6c9
WebbYang <yp1369yang@gmail.com>
2020-08-24 23:03:28 +0800

    Update settings.py
    Hide secrete key

--- Lecture_3/lecture3/lecture3/settings.py
+++ Lecture_3/lecture3/lecture3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j^=k7)c1u+7gu%m1*=2oa3wnk(try(mo(b7f7smmt@11rc9f^%'
+SECRET_KEY = 'Your secret key.'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
